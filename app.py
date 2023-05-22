c416df4b7bf9171b4dcf45237d29ba715664f900
Likhith_Reddy <112001021@smail.iitpkd.ac.in>
2023-05-22 22:33:07 +0530

    app updated

--- app.py
+++ app.py
@@ -322,12 +322,11 @@ def advisoryhome() :
                     ssp1 = (60-phosphorus-(0.46*dap1))/0.16
                 else:
                     ssp1 = (60-phosphorus)/0.16
-            st.write(ssp1)
             mop1 = (40-potassium)/0.6 if potassium < 40 else 0
-            if urea1 > 0: fertilizer.append(f"Urea : {round(urea1*area,2)} Kg/ha")
-            if dap1 > 0: fertilizer.append(f"Di-ammonium Phosphate[DAP] : {round(dap1*area,2)} Kg/ha")
-            # if ssp1 > 0: fertilizer.append(f"Single superphosphate[SSP] : {ssp1} Kg/ha")
-            if mop1 > 0: fertilizer.append(f"Muriate of Potash[MOP] : {round(mop1*area,2)} Kg/ha")
+            if urea1 and urea1 > 0: fertilizer.append(f"Urea : {round(urea1*area,2)} Kg/ha")
+            if dap1 and dap1 > 0: fertilizer.append(f"Di-ammonium Phosphate[DAP] : {round(dap1*area,2)} Kg/ha")
+            if ssp1 and ssp1 > 0: fertilizer.append(f"Single superphosphate[SSP] : {ssp1} Kg/ha")
+            if mop1 and mop1 > 0: fertilizer.append(f"Muriate of Potash[MOP] : {round(mop1*area,2)} Kg/ha")
             st.info("Basal Application :")
             i=0
             for fert in fertilizer:
